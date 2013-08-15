183d1ea49da270a5326cf255a1911312c58903b5
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-08-15 15:29:49 -0400

    vfp: compile vfp with NEON (Chad Goodman), patched to linux 3.0.97
    Conflicts:
    	.config
    	.version
    	include/generated/compile.h

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Wed Sep 11 22:37:12 EDT 2013"
+#define LINUX_COMPILE_BY "bsmitty2"
+#define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
+#define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
