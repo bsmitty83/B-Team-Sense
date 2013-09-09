0dd28bd55ea936b3af89f666170d915030be0a11
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-09-08 20:14:46 -0400

    patch to linux 3.0.40

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -23,6 +23,7 @@
 #include <linux/slab.h>
 #include <linux/suspend.h>
 #include <linux/syscore_ops.h>
+#include <linux/ftrace.h>
 #include <trace/events/power.h>
 
 #include "power.h"
@@ -214,6 +215,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 	}
 	if (!suspend_console_deferred)
 		suspend_console();
+		ftrace_stop();
 	suspend_test_start();
 	error = dpm_suspend_start(PMSG_SUSPEND);
 	if (error) {
@@ -231,6 +233,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 	dpm_resume_end(PMSG_RESUME);
 	suspend_test_finish("resume devices");
 	if (!suspend_console_deferred)
+	ftrace_stop();
 		resume_console();
  Close:
 	if (suspend_ops->end)
