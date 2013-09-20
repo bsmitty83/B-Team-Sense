40859e8bfc973702379ab2493611903e247857d8
Bryan Maltais <brymaster5000@gmail.com>
2013-09-19 21:55:53 -0400

    added zen ioscheduler

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 7 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Thu Sep 19 21:38:03 EDT 2013"
+#define UTS_VERSION "#7 PREEMPT Thu Sep 19 21:53:21 EDT 2013"
 #define LINUX_COMPILE_BY "brymaster5000"
 #define LINUX_COMPILE_HOST "bryslaptop"
 #define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
